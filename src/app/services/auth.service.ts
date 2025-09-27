97c121278c52cc59edb5b4e4860e90c325b48d3b
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-09-27 14:54:59 +0200

    Add AuthService with registration and login methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+  private apiUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  register(user: Partial<User>): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/register`, user);
+  }
+
+  login(credentials: { email: string; password: string }): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/login`, credentials);
+  }
 }
