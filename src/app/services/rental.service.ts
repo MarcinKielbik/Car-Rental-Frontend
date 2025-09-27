369c476b25c54af0e0d6a91cf2f24571532e5a97
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-09-27 14:54:10 +0200

    Add RentalService with methods to get user rentals and create rentals

--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Rental } from '../models/rental.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RentalService {
-  
+  private apiUrl = 'http://localhost:8080/api/rentals';
+
+  constructor(private http: HttpClient) {}
+
+  getUserRentals(userId: number): Observable<Rental[]> {
+    return this.http.get<Rental[]>(`${this.apiUrl}/user/${userId}`);
+  }
+
+  createRental(rental: Partial<Rental>): Observable<Rental> {
+    return this.http.post<Rental>(this.apiUrl, rental);
+  }
 }
