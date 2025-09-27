dc580b67591e7a1205b02eda6f09acae81ea90e3
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-09-27 19:15:43 +0200

    Add guards

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { Observable } from 'rxjs/internal/Observable';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,24 @@ export class AuthService {
   }
 
   login(credentials: { email: string; password: string }): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/login`, credentials);
+    return this.http.post<User>(`${this.apiUrl}/login`, credentials).pipe(
+      tap(user => {
+        localStorage.setItem('user', JSON.stringify(user));
+      })
+    );
   }
+
+  getCurrentUser(): any {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+}
+
+isLoggedIn(): boolean {
+  return !!localStorage.getItem('user');
+}
+
+logout(): void {
+  localStorage.removeItem('user');
+}
+
 }
